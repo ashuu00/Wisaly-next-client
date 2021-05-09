e83b90269f47188c35a5611fa64108f73766db6b
ashuu00 <ashutosh.anurag2262@gmail.com>
2021-05-09 14:14:17 +0530

    changed gtag

--- pages/_document.js
+++ pages/_document.js
@@ -28,14 +28,24 @@ class MyDocument extends Document {
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
 <meta name="theme-color" content="#ffffff"/>
 {/*google analytics tag */}
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8965ZF1E7N"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'G-8965ZF1E7N');
-</script>
+ {/* Google Tag Manager */}
+ <script
+        dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','G-8965ZF1E7N');`,
+        }}
+    />
+    {/* End Google Tag Manager */}
+    {/* Google Tag Manager (noscript) */}
+    <noscript
+        dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-8965ZF1E7N" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+    />
+    {/* End Google Tag Manager (noscript) */}
         </Head>
         <body>
           <Main />
