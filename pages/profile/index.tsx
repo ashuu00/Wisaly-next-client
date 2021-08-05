1e93e8763496c14cbd4fdd355d71d7c6f7ade925
ashuu00 <ashutosh.anurag2262@gmail.com>
2021-08-05 13:15:57 +0530

    added changes

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/dist/client/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function index() {
     const router = useRouter();
     //@ts-ignore
     const userName = useSelector((state) => state.userState).username;
-    if (userName === '' || userName === undefined) router.push('/signup');
-    else router.push(`/profile/${userName}`);
+    useEffect(() => {
+        if (userName === '' || userName === undefined) router.push('/signup');
+        else router.push(`/profile/${userName}`);
+    });
     return <div></div>; //this changed
 }
