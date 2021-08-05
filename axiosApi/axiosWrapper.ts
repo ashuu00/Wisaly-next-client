baa52225c7a530756f07700126322c4478980553
ashuu00 <ashutosh.anurag2262@gmail.com>
2021-08-05 21:51:46 +0530

    added https backend

--- axiosApi/axiosWrapper.ts
+++ axiosApi/axiosWrapper.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import cookieCutter from 'cookie-cutter';
 
 const test = false;
-const prefix = test ? 'http://localhost:8080/api/v1' : 'http://13.233.73.34/api/v1';
+const prefix = test ? 'http://localhost:8080/api/v1' : 'https://api.wisaly.com/api/v1';
 
 export const axiosGet = async (url: string, token?: String, options?: any) => {
     console.log('method called');
